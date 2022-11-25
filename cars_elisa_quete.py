8817b17514c3331683bed245050276802ec941fd
elisabeth <96701818+Eli-sa-1983@users.noreply.github.com>
2022-11-25 15:02:09 +0100

    Add files via upload

---
+++ cars_elisa_quete.py
@@ -0,0 +1,15 @@
+import streamlit as st
+import pandas as pd
+import seaborn as sns
+
+st.title('Hello Wilders, welcome to my application!')
+
+st.write("I enjoy to discover stremalit possibilities")
+
+link = "https://raw.githubusercontent.com/murpi/wilddata/master/quests/cars.csv"
+df_cars = pd.read_csv(link)
+df_cars
+
+#graphique
+st.line_chart(df_cars['cubicinches'])
+
