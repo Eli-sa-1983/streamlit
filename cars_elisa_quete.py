6ef2f7e06f80c5b17aedfaaa2d7113854a8c791e
elisabeth <96701818+Eli-sa-1983@users.noreply.github.com>
2022-11-26 13:29:48 +0100

    Update cars_elisa_quete.py

--- cars_elisa_quete.py
+++ cars_elisa_quete.py
@@ -1,8 +1,8 @@
 import streamlit as st
 import requests 
 import pandas as pd
-#import seaborn as sns
-#import matplotlib.pyplot as plt
+import seaborn as sns
+import matplotlib.pyplot as plt
 import plotly.express as px
 
 
