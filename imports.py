c135e109dc86fd8b1665755d0a5c9e62dab92f1c
elisabeth <96701818+Eli-sa-1983@users.noreply.github.com>
2022-11-26 13:42:36 +0100

    Update imports.py

--- imports.py
+++ imports.py
@@ -1,6 +1,11 @@
-seaborn==0.11.2
-pandas==1.4.2
-streamlit==1.13.0
-matplotlib==3.5.1
-request==2.27.1
+numpy==1.19.2
+seaborn==0.11.0
+pandas==1.1.3
+streamlit==0.79.0
+
+#seaborn==0.11.2
+#pandas==1.4.2
+#streamlit==1.13.0
+#matplotlib==3.5.1
+#request==2.27.1
 
