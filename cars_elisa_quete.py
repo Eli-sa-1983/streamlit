b57187bd09b7be20a9eaf78b7f8f8b08b123b461
elisabeth <96701818+Eli-sa-1983@users.noreply.github.com>
2022-11-29 21:09:47 +0100

    Update cars_elisa_quete.py

--- cars_elisa_quete.py
+++ cars_elisa_quete.py
@@ -44,15 +44,15 @@ def page2():
 def page3():
     st.markdown("Japon")
     st.write(df_cars[df_cars['continent']==' Japan.'])
-    st.sidebar.markdown("Soleil Levant")
     st.sidebar.image('flag_japan.jpeg',width=200)
+    st.sidebar.markdown("Soleil Levant")
     
     
 def page4():
     st.markdown("Europe")
     st.write(df_cars[df_cars['continent']==' Europe.'])
-    st.sidebar.markdown("Vieux Continent")
     st.sidebar.image('flag_europe.jpeg',width=200)
+    st.sidebar.markdown("Vieux Continent")
     
 
 page_names_to_funcs = {
