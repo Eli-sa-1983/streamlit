4f2306c36260bd44347d661e60e0fb6bc245e954
elisabeth <96701818+Eli-sa-1983@users.noreply.github.com>
2022-11-29 14:00:17 +0100

    Update cars_elisa_quete.py

--- cars_elisa_quete.py
+++ cars_elisa_quete.py
@@ -31,7 +31,7 @@ def page2():
     st.markdown("USA")
     st.line_chart(df_cars['continent'])
     st.write(df_cars[df_cars['continent']==' US.'])
-    st.image('flag_usa.jpeg'width=600)
+    st.image('flag_usa.jpeg',width=600)
 	
 
    
